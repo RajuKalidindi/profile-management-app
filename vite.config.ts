8022bf58b42cc68a2a7acf0e1af03a6046a05917
Raju Kalidindi <krkraju997@gmail.com>
2024-10-13 20:52:38 +0530

    Deployment attempt 3

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,13 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: process.env.VITE_BASE_PATH || "/",
+	server: {
+		proxy: {
+			"/api": {
+				target: "http://localhost:3000",
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/api/, ""),
+			},
+		},
+	},
 });
